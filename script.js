0495e7ed1ed1629a5008c43164e0f80a5930a78c
Marc Walter <m.walter-dev@gmx.de>
2022-09-29 22:38:24 +0200

    show winner

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let fields = ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none', 'none' ]
+let fields = ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none', 'none']
 let currentPlayer = [0];
 let currentShape = 'cross';
 
@@ -38,15 +38,30 @@ function showShape() {
 
 function checkWinner() {
     let amountColumns = 3;
-    for (let i = 0; i < amountColums; i++) {
-        if (fields[i] == fields[i + amountColumns] && fields[i] == fields[i + amountColumns * 2] && fields[i] != 'none') {
-            console.log('winner');
+    let amountRows = 3;
+    let amountFields = amountColumns * amountRows;
+    let winner;
+    for (let i = 0; i < amountFields - (2 * amountColumns); i++) {
+        if (checkComumn(i, amountColumns)) {
+            winner = fields[i];
         }
     }
     for (let i = 0; i < 7; i += 3) {
         if (fields[i] == fields[i + 1] && fields[i] == fields[i + 2] && fields[i] != 'none') {
-            console.log('winner');
+            winner = fields[i];
         }
     }
+    if (fields[4] == fields[0] && fields[4] == fields[8] && fields[4] != 'none') {
+        winner = fields[4];
+    }
+    if (fields[4] == fields[2] && fields[4] == fields[6] && fields[4] != 'none') {
+        winner = fields[4];
+    }
+    if (winner) {
+        console.log('Gewonnen hat', winner);
+    }
+}
 
+function checkComumn(i, amountColumns) {
+    return fields[i] == fields[i + amountColumns] && fields[i] == fields[i + amountColumns * 2] && fields[i] != 'none'
 }
