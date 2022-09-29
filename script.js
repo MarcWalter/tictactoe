f1235a85cc117d792aee1fc66cecb8c79c99d1f8
Marc Walter <m.walter-dev@gmx.de>
2022-09-29 15:22:19 +0200

    checkWinner row and column

---
+++ script.js
@@ -0,0 +1,52 @@
+let fields = ['none', 'none', 'none', 'none', 'none', 'none', 'none', 'none', 'none' ]
+let currentPlayer = [0];
+let currentShape = 'cross';
+
+
+function fillShape(id) {
+    fields[id] = currentShape;
+    showShape();
+    changePlayer();
+
+    console.log(currentPlayer);
+    console.log(fields);
+    checkWinner();
+}
+
+function changePlayer() {
+    if (currentPlayer == 0) {
+        currentPlayer = 1;
+        currentShape = 'circle';
+    } else {
+        currentPlayer = 0;
+        currentShape = 'cross';
+    }
+
+}
+
+function showShape() {
+    for (let i = 0; i < fields.length; i++) {
+        const element = fields[i];
+        if (element == 'cross') {
+            document.getElementById('cross-' + i).classList.remove('d-none');
+        }
+        if (element == 'circle') {
+            document.getElementById('circle-' + i).classList.remove('d-none');
+        }
+    }
+}
+
+function checkWinner() {
+    let amountColumns = 3;
+    for (let i = 0; i < amountColums; i++) {
+        if (fields[i] == fields[i + amountColumns] && fields[i] == fields[i + amountColumns * 2] && fields[i] != 'none') {
+            console.log('winner');
+        }
+    }
+    for (let i = 0; i < 7; i += 3) {
+        if (fields[i] == fields[i + 1] && fields[i] == fields[i + 2] && fields[i] != 'none') {
+            console.log('winner');
+        }
+    }
+
+}
